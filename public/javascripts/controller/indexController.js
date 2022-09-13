2fcf65480e222adf280ee235f3b4a2f79d7af50d
Hazal Beyazal <hazal.beyazal@solviads.com>
2022-09-13 14:14:44 +0300

    'newUser'

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,22 @@
+app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
+
+    $scope.init = () =>{
+        const username = prompt('lütfen kullanıcı adı giriniz: ');
+        if(username)
+            initSocket(username)
+        else
+        return false;
+    };
+    
+    function initSocket(username){
+        indexFactory.connectSocket('http://localhost:3000',{
+            reconnectionAttempts:3,
+            reconnectionDelay:600
+        }).then((socket)=>{
+            socket.emit( 'newUser', { username } )
+        }).catch((err)=>{
+            console.log(err)
+        })
+    };
+   
+}]);
